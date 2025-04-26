3bb98eb8b5d830f5e355adcf3fa17866b57cc828
Emad Rahman <emad__rahman@hotmail.com>
2025-04-25 22:27:27 -0400

    Trained New Model

---
+++ ClothingDetector/datasets/Clothing-Detection-10/README.dataset.txt
@@ -0,0 +1,6 @@
+# Clothing Detection > 2025-04-24 11:17pm
+https://universe.roboflow.com/niceworkspace/clothing-detection-uhmgf
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
--- ClothingDetector/datasets/modeltraining.py
+++ ClothingDetector/datasets/modeltraining.py
@@ -1,7 +1,7 @@
 from roboflow import Roboflow
 rf = Roboflow(api_key="Fl6OJur3jHyAJR2tyUfM")
 project = rf.workspace("niceworkspace").project("clothing-detection-uhmgf")
-version = project.version(9)
-#dataset = version.download("yolov11")
+version = project.version(10)
+dataset = version.download("yolov11")
                 
-project.version(version.version).deploy(model_type="yolov11", model_path="C:/Users/emad_/Downloads/ClothingDetector/runs/detect/train8")
+#project.version(version.version).deploy(model_type="yolov11", model_path="C:/Users/emad_/Downloads/ClothingDetector/runs/detect/train8")
